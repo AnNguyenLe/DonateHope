78ecc1439dd1c3334f4189374a904e96142547f0
FangHaiWu <164314841+FangHaiWu@users.noreply.github.com>
2024-11-15 19:10:47 +0700

    Implement API for Campaign Comment

---
+++ src/DonateHope.Infrastructure/Repositories/CampaignCommentsRepository.cs
@@ -0,0 +1,153 @@
+using System.Linq.Expressions;
+using Dapper;
+using DonateHope.Core.Errors;
+using DonateHope.Domain.Entities;
+using DonateHope.Domain.RepositoryContracts;
+using DonateHope.Infrastructure.Data;
+using DonateHope.Infrastructure.DbContext;
+using FluentResults;
+using Microsoft.EntityFrameworkCore;
+namespace DonateHope.Infrastructure.Repositories;
+
+public class CampaignCommentsRepository(IDbConnectionFactory dbConnectionFactory, ApplicationDbContext applicationDbContext) : ICampaignCommentsRepository
+{
+    private readonly IDbConnectionFactory _dbConnectionFactory = dbConnectionFactory;
+    private readonly ApplicationDbContext _dbContext = applicationDbContext;
+
+    public async Task<int> AddCampaignComment(CampaignComment campaignComment)
+    {
+        using var dbConnection = await _dbConnectionFactory.CreateConnectionAsync();
+        var sqlCommand = """
+                INSERT INTO campaign_comments 
+                    (
+                        id,
+                        content, 
+                        created_at, 
+                        updated_at, 
+                        created_by, 
+                        updated_by,
+                        is_deleted,
+                        deleted_at,
+                        deleted_by,
+                        is_banned, 
+                        user_id,
+                        campaign_id
+                    )
+                VALUES 
+                    (
+                        @Id,
+                        @Content,  
+                        @CreatedAt,     
+                        @UpdatedAt,
+                        @CreatedBy,
+                        @UpdatedBy,
+                        @IsDeleted,
+                        @DeletedAt,
+                        @DeletedBy,
+                        @IsBanned,
+                        @UserId,
+                        @CampaignId
+                    );
+            """;
+
+        // TODO: Update campaign_log also
+        return await dbConnection.ExecuteAsync(sqlCommand, campaignComment);
+    }
+
+    public IQueryable<CampaignComment> GetCampaignComments(Expression<Func<CampaignComment, bool>> predicate)
+    {
+        return _dbContext.CampaignComments.Where(predicate);
+    }
+
+    public async Task<Result<CampaignComment>> GetCampaignCommentById(Guid campaignCommentId)
+    {
+        using var dbConnection = await _dbConnectionFactory.CreateConnectionAsync();
+        var queryResult = await _dbContext
+            .CampaignComments.Where(cc => cc.Id == campaignCommentId)
+            .FirstOrDefaultAsync();
+        if (queryResult is null)
+        {
+            return new ProblemDetailsError($"Campaign Comment with ID: {campaignCommentId} not found.");
+        }
+
+        return queryResult;
+    }
+
+    public async Task<Result<int>> UpdateCampaignComment(CampaignComment updateCampaignComment)
+    {
+        using var dbConnection = await _dbConnectionFactory.CreateConnectionAsync();
+        var sqlCommand = """
+                         UPDATE campaign_comments
+                         SET
+                             content = @Content,
+                             updated_at = @UpdatedAt,
+                             updated_by = @UpdatedBy
+                         WHERE 
+                             id = @Id;
+                             
+                         """;
+        var totalAffectedRows = await dbConnection.ExecuteAsync(sqlCommand, updateCampaignComment);
+        if (totalAffectedRows == 0)
+        {
+            return new ProblemDetailsError("Unable to update campaign_contribution.");
+        }
+        return totalAffectedRows;
+    }
+
+
+    public async Task<Result<int>> DeleteCampaignComment(
+        Guid campaignCommentId,
+        Guid deletedBy,
+        string reasonForDeletion
+        )
+    {
+        using var dbConnection = await _dbConnectionFactory.CreateConnectionAsync();
+        var sqlCommand = """
+                         UPDATE campaign_comments
+                         SET
+                            is_deleted = @isDeleted,
+                            deleted_at = @deletedAt,
+                            deleted_by = @deletedBy,
+                            reason_for_deletion = @reasonForDeletion
+                         WHERE id = @campaignCommentId
+                         """;
+        var totalAffectedRows = await dbConnection.ExecuteAsync(
+            sqlCommand,
+            new
+            {
+                isDeleted = true,
+                deletedAt = DateTime.UtcNow,
+                deletedBy,
+                reasonForDeletion,
+                campaignCommentId
+            });
+        if (totalAffectedRows == 0)
+        {
+            return new ProblemDetailsError("Something wrong trying to delete this record.");
+        }
+
+        return totalAffectedRows;
+    }
+
+    /// <summary>
+    /// USING THIS WITH CAUTION! Your data will be deleted permanently and will not be able to recovered!
+    /// </summary>
+    public async Task<Result<int>> DeleteCampaignCommentPermanently(Guid campaignCommentId)
+    {
+        using var dbConnection = await _dbConnectionFactory.CreateConnectionAsync();
+        var sqlCommand = """
+                         DELETE campaign_comments
+                         WHERE id = @campaignCommentId;
+                         """;
+        var totalAffectedRows = await dbConnection.ExecuteAsync(sqlCommand, new { campaignCommentId });
+        if (totalAffectedRows == 0)
+        {
+            return new ProblemDetailsError(
+                $"Unable to permanently delete campaign_contribution with ID: {campaignCommentId}"
+            );
+        }
+        return totalAffectedRows;
+    }
+
+
+}
---
+++ src/DonateHope.WebAPI/Controllers/v1/CampaignComment/CampaignCommentController.cs
@@ -0,0 +1,158 @@
+using Asp.Versioning;
+using DonateHope.Core.Common.ExtensionMethods;
+using DonateHope.Core.ConfigurationOptions.AppServer;
+using DonateHope.Core.DTOs.CampaignCommentDTOs;
+using DonateHope.Core.Mappers;
+using DonateHope.Core.ServiceContracts.CampaignCommentServiceContracts;
+using DonateHope.Domain.IdentityEntities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+
+namespace DonateHope.WebAPI.Controllers.v1.CampaignComment;
+
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/campaign-comment")]
+[ApiController]
+public class CampaignCommentController(
+    ICampaignCommentCreateService campaignCommentCreateService,
+    ICampaignCommentRetrieveService campaignCommentRetrieveService,
+    ICampaignCommentUpdateService campaignCommentUpdateService,
+    ICampaignCommentDeleteService campaignCommentDeleteService,
+    UserManager<AppUser> userManager,
+    IOptions<MyAppServerConfiguration> myAppServerConfiguration,
+    CampaignCommentMapper campaignCommentMapper
+) : CustomControllerBase
+{
+    private readonly UserManager<AppUser> _userManager = userManager;
+    private readonly CampaignCommentMapper _campaignCommentMapper = campaignCommentMapper;
+    private readonly MyAppServerConfiguration _app = myAppServerConfiguration.Value;
+    private readonly ICampaignCommentCreateService _campaignCommentCreateService = campaignCommentCreateService;
+    private readonly ICampaignCommentRetrieveService _campaignCommentRetrieveService = campaignCommentRetrieveService;
+    private readonly ICampaignCommentUpdateService _campaignCommentUpdateService = campaignCommentUpdateService;
+    private readonly ICampaignCommentDeleteService _campaignCommentDeleteService = campaignCommentDeleteService;
+
+    [HttpPost("create", Name = nameof(CreateCampaignComment))]
+    public async Task<ActionResult<CampaignCommentGetResponseDto>> CreateCampaignComment(
+        [FromBody] CampaignCommentCreateRequestDto createRequest
+    )
+    {
+        var userId = _userManager.GetUserId(User);
+
+        if (userId is null)
+        {
+            return BadRequestProblemDetails("Unable to identify user");
+        }
+        if (!Guid.TryParse(userId, out var parsedUserId))
+        {
+            return BadRequestProblemDetails("Unable to identify user");
+        }
+        var result = await _campaignCommentCreateService.CreateCampaignCommentAsync(createRequest, userId);
+
+        if (result.IsFailed)
+        {
+            return result.Errors.ToDetailedBadRequest();
+        }
+
+        var campaignComment = result.Value;
+
+        return CreatedAtRoute(
+            nameof(CreateCampaignComment),
+            new { id = campaignComment.Id },
+            campaignComment
+
+        );
+    }
+
+    [HttpGet("{id}", Name = nameof(GetCampaignComment))]
+    public async Task<ActionResult<CampaignCommentGetResponseDto>> GetCampaignComment([FromRoute] Guid id)
+    {
+        var userId = _userManager.GetUserId(User);
+        if (userId is null)
+        {
+            return BadRequestProblemDetails("Unable to identify user");
+        }
+
+        var result = await _campaignCommentRetrieveService.GetCampaignCommentById(id);
+
+        if (result.IsFailed)
+        {
+            return result.Errors.ToDetailedBadRequest();
+        }
+
+        return result.Value;
+    }
+    [HttpPut("{id}", Name = nameof(UpdateCampaignComment))]
+    public async Task<ActionResult<CampaignCommentUpdateRequestDto>> UpdateCampaignComment([FromRoute] string id, [FromBody] CampaignCommentUpdateRequestDto updateCampaignCommentDto)
+    {
+        if (!Guid.TryParse(id, out var campaignCommentId))
+        {
+            return BadRequestProblemDetails("Invalid ID format.");
+        }
+
+        if (campaignCommentId != updateCampaignCommentDto.Id)
+        {
+            return BadRequestProblemDetails("Campaign Comment ID does not match.");
+        }
+
+        var userId = _userManager.GetUserId(User);
+
+        if (userId is null)
+        {
+            return BadRequestProblemDetails("Unable to identify user.");
+        }
+
+        if (!Guid.TryParse(userId, out var parsedUserId))
+        {
+            return BadRequestProblemDetails("Unable to identify user.");
+        }
+
+        var updatedResult = await _campaignCommentUpdateService.UpdateCampaignCommentAsync(
+            updateCampaignCommentDto,
+            parsedUserId
+        );
+
+        if (updatedResult.IsFailed)
+        {
+            return updatedResult.Errors.ToDetailedBadRequest();
+        }
+
+        return NoContent();
+        return NoContent();
+    }
+    [HttpDelete("{id}", Name = nameof(DeleteCampaignComment))]
+    public async Task<ActionResult<CampaignCommentDeleteDto>> DeleteCampaignComment(
+        [FromRoute] string id,
+        [FromBody] CampaignCommentDeleteRequestDto reasonForDeletionRequestDto)
+    {
+        if (!Guid.TryParse(id, out var campaignCommentId))
+        {
+            return BadRequestProblemDetails("Invalid ID format");
+        }
+        var userId = _userManager.GetUserId(User);
+
+        if (userId is null)
+        {
+            return BadRequestProblemDetails("Unable to identify user");
+        }
+
+        if (!Guid.TryParse(userId, out var deletedBy))
+        {
+            return BadRequestProblemDetails("Invalid user identification");
+        }
+
+        var result = await _campaignCommentDeleteService.DeleteCampaignCommentAsync(
+            campaignCommentId,
+            deletedBy,
+            reasonForDeletionRequestDto.ReasonForDeletion
+            );
+
+        if (result.IsFailed)
+        {
+            return result.Errors.ToDetailedBadRequest();
+        }
+        return Ok();
+       return Ok();
+    }
+}
