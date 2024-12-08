a9fddadfc5c0e962e419eeec0d5d80a077cba537
FangHaiWu <vuhuyphuong8x@gmail.com>
2024-12-08 20:10:44 +0700

    sua Campaign comment, cap nhat them function cho phep hien thi tat ca comment cua 1 campaign

--- src/DonateHope.Core/ServiceContracts/CampaignCommentServiceContracts/ICampaignCommentRetrievalService.cs
+++ src/DonateHope.Core/ServiceContracts/CampaignCommentServiceContracts/ICampaignCommentRetrievalService.cs
@@ -6,4 +6,5 @@ namespace DonateHope.Core.ServiceContracts.CampaignCommentServiceContracts;
 public interface ICampaignCommentRetrieveService
 {
     Task<Result<CampaignCommentGetResponseDto>> GetCampaignCommentById(Guid campaignCommentId);
+    Task<Result<IEnumerable<CampaignCommentGetResponseDto>>> GetCommentsByCampaignId(Guid campaignId);
 }
